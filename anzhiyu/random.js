4eee23433fcdd23c1d733e18996d8683685d0f4d
Tian yaojie <1687572057qq.com>
2026-03-24 11:12:35 +0800

    Site updated: 2026-03-24 11:12:34

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/20/Test1/h","2026/03/20/Test2/h","2026/03/20/Test3/h","2026/03/20/hello-world/h"];function toRandomPost(){
+var posts=["2026/03/20/Test3/h","2026/03/20/Test2/h","2026/03/20/hello-world/h","2026/03/24/数模/h"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
