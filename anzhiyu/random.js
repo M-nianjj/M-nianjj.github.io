5fa4aca2c023324e20d49abe8e69c9f5826e0a8c
Tian yaojie <1687572057qq.com>
2026-04-11 16:30:32 +0800

    Site updated: 2026-04-11 16:30:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/20/Test2/h","2026/03/20/hello-world/h","2026/04/07/Sci-VLA/h","2026/04/01/ToolRosetta/h","2026/03/20/Test3/h","2026/03/24/数模/h"];function toRandomPost(){
+var posts=["2026/03/20/Test2/h","2026/03/20/Test3/h","2026/03/20/hello-world/h","2026/04/07/Sci-VLA/h","2026/03/24/数模/h","2026/04/01/ToolRosetta/h"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
